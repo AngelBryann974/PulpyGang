1e8382b3cfb0ce67c28c432b206a32d696d2ed3e
angelbryann974 <angelbryann974@gmail.com>
2019-06-19 12:24:27 -0500

    PrimeraVersion

---
+++ Assets/Assets/Controlador.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controlador : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void CambiarEscena(string nombre)
+    {
+        print("Cambiando a la escena" + nombre);
+        SceneManager.LoadScene(nombre);
+    }
+
+    public void Salir()
+    {
+        print("Saliendo del Juego");
+        Application.Quit();
+    }
+}
---
+++ Assets/Assets/Scripts/CdeEscena1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CdeEscena1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnBecameInvisible()
+    {
+        SceneManager.LoadScene("Portada");
+    }
+}
